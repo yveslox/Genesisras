f21bdda39756df4dc6a37c57704b275a35c2fe27
yveslox <yvesmole@gmail.com>
2022-11-08 15:51:11 +0100

    upgrade 08/11/2022

---
+++ java-programs/graphics/draw-round2.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+
+class draw_round2
+ {    
+     
+     public  void paint(Graphics g)
+     { 
+    
+        //SetBackground(Color.yellow);
+        g.drawLine(20,20,180,80);
+     }
+ }
---
+++ java-programs/graphics/fill-oval.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+
+class fill_oval
+ {    
+     
+     public  void paint(Graphics g)
+     { 
+    
+        //SetBackground(Color.yellow);
+        g.drawLine(20,20,180,80);
+     }
+ }
