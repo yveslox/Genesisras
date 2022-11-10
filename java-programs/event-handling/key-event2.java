485e91974924488ed5a0af0280980528bafe8207
yveslox <yvesmole@gmail.com>
2022-11-10 14:34:12 +0100

    upgrade 10/11/2022

---
+++ java-programs/event-handling/key-event2.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+class key_event2   
+ {    
+     
+     public  void paint(Graphics g)
+     { 
+    
+        //SetBackground(Color.yellow);
+        g.drawLine(20,20,180,80);
+     }
+ }
---
+++ java-programs/event-handling/mouse-adapter2.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+class mouse_adapter2   
+ {    
+     
+     public  void paint(Graphics g)
+     { 
+    
+        //SetBackground(Color.yellow);
+        g.drawLine(20,20,180,80);
+     }
+ }
