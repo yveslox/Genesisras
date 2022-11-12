8bdef42a1fffa1729498fc7f6ce2da70fc965875
yveslox <yvesmole@gmail.com>
2022-11-12 14:25:51 +0100

    upgrade 12/11/2022

---
+++ java-programs/awt/checkbox-group.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+class checkbox_groupe     
+ {    
+     
+     public  void paint(Graphics g)
+     { 
+    
+        //SetBackground(Color.yellow);
+        g.drawLine(20,20,180,80);
+     }
+ }
---
+++ java-programs/awt/list.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+class list     
+ {    
+     
+     public  void paint(Graphics g)
+     { 
+    
+        //SetBackground(Color.yellow);
+        g.drawLine(20,20,180,80);
+     }
+ }
