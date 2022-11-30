b7ec91139c048166ea519d6bca7fc419bedbb471
yveslox <yvesmole@gmail.com>
2022-11-30 12:43:51 +0100

    upgrade 30/11/2022

---
+++ java-programs/mix/gregorian.java
@@ -0,0 +1,24 @@
+import java.util.Calendar;
+//import.java.util.GregorianCalendar;
+
+class gregorian 
+{
+    public static void main(String args[])
+     {
+     // GregorianCalendar gc = new GregorianCalendar();
+     // GregorianCalendar gc2 = new GregorianCalendar(2012,0,16,05,15,30);
+     /**  System.out.println(gc.get(Calendar.DATE));
+      System.out.prinln("gc2 ="+gc2);
+      System.out.println(gc2.get(Calendar.DATE)+"/"+(gc2.get(Calendar.MONTH)));
+      
+      if(gc.isLeapYear(2012))
+      {
+        System.out.println("yes");
+      }
+      else
+      {
+        System.out.println("no");
+      }***/
+      
+    }
+}
