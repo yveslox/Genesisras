755ebc0b3b85d44f9eaccfa977230047a7026d36
yveslox <yvesmole@gmail.com>
2022-12-19 12:59:44 +0100

    19/12/2022

---
+++ practice-code-purple/c-programming/pointer-comp2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int *ptrA;
+    float *ptrB;
+    
+    ptrA = (int *)1000;
+    ptrB = (float *)2000;
+    
+    
+    //if(ptrB > ptrA)
+        printf("PtrB is greater than ptrA\n");
+   
+    return 0;
+}
