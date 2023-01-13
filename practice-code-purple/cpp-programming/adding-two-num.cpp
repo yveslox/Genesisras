de19a9bbacafbc7a5bf1ad45593de2eab8ee15f7
yveslox <yvesmole@gmail.com>
2023-01-13 14:36:08 +0100

    13/01/2023

---
+++ practice-code-purple/cpp-programming/adding-two-num.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    int sum = 0;
+
+    cout<<"=== program to add 2 numbers ===\n";
+
+    cout<<"Enter the first number : ";
+    cin>>a;
+
+    cout<<"Enter the second number : ";
+    cin>>b;
+
+    sum = a + b;
+
+    cout<<"Addition of two numbers is : "<<sum<<endl;
+
+}
---
+++ practice-code-purple/cpp-programming/hello-world.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello word !\n Welcome\n";
+    return 0;
+}
