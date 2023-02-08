03cc044b8d1cf16b742ce8492908c42fa88a667e
yveslox <yvesmole@gmail.com>
2023-02-08 12:05:51 +0100

    08/02/2023

---
+++ practice-code-purple/cpp-programming/floyd-triangle.cpp
@@ -0,0 +1,35 @@
+#include<math.h>
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+/* Main function */
+int main()
+{
+    int i,j,rows,cols;
+    
+    int n=1,first,last;
+
+    cout<<"Enter number of rows in pyramid: ";
+    cin>>rows;
+
+    cout<<"The required pyramid pattern containing "<<rows<<" rows is :"<<endl;
+
+    for(i = 1;i <= rows;i++)
+    {
+     first =n;
+     last = first + i - 1;
+     cols =i;
+    
+    //cout<<"Row #"<<i<<" contains "<<(rows - i +1)<<" stars : ";
+     
+     for(j=1;j <= cols;j++)
+     {
+        cout<<n<<" ";
+        n+=1;
+     }
+     cout<<"\n";
+    }
+    return 0;
+}
