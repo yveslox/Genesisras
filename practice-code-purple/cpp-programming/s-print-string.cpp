ff9282f439c9f800ab368fc325dfbc52cfad21d9
yveslox <yvesmole@gmail.com>
2023-03-29 15:26:51 +0200

    29/03/2023

---
+++ practice-code-purple/cpp-programming/s-print-string.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+#include<bits/stdc++.h>
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+    char a[20];
+    int i,j;
+
+    cout<<"Enter the string : ";
+    
+    cin>>a;
+
+    cout<<"Your entered String is : ";
+    for(i=0;i<20;i++)
+    {
+        cout<<a[i];
+    }
+
+    cout<<endl;
+
+    return 0;
+    
+}
