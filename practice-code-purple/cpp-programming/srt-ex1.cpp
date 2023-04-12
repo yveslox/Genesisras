09242b5045571c25a4cd3514e15efc6ae1e72ffc
yveslox <yvesmole@gmail.com>
2023-04-12 14:04:27 +0200

    12/04/2023

---
+++ practice-code-purple/cpp-programming/srt-ex1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int bubble_sort(int n,int array[])
+{
+    int temp;
+    for(int i = 0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j--)
+        {
+            if(array[j]>array[j+1])
+            {
+                temp = array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int arr[]={5,6,9,2,3};
+    int n = sizeof(arr)/(sizeof(arr[0]));
+    bubble_sort(n,arr);
+    cout<<"Elements eftare sorting of array : "<<endl;
+    for(int i = 0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
