2370de092b0964d3f39b74de44eeca9dc3ffffa1
yveslox <yvesmole@gmail.com>
2023-07-08 10:06:58 +0200

    08/07/2023

---
+++ pro-language-droid/c-prog/data-type.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int num=15;float f1 = 12.5;char ch ='z';char name[]="john";
+
+    printf("%d\n",num);
+    printf("%f\n",f1);
+    printf("%c\n",ch);
+    printf("%s\n",name);
+}
