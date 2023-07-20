ffe0f8de0460d1fd14e1ba6c342f322e2f44d9d1
yveslox <yvesmole@gmail.com>
2023-07-20 12:32:57 +0200

    20/07/2023

---
+++ pro-language-droid/c-prog/type-casting.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 7;
+    int res_integer = num1/num2;
+    float res_float =(float)num1/num2;
+    
+    printf("res_integer : %d\n",res_integer);
+    printf("res_float : %f\n",res_float);
+    
+    int charValue ='a';
+
+    printf("CharValue : %d\n",charValue);
+}
