bb850be9e519ebc44c71756807f24a57ed645863
yveslox <yvesmole@gmail.com>
2023-07-26 12:13:52 +0200

    25/07/2023

---
+++ pro-language-droid/cpp-prog/input-output.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter a number : ";
+    cin>>num;
+    cout<<"Enter number is "<<num<<endl;
+
+    char name[50];
+    cout<<"Enter your name : ";
+    cin>>name;
+    cout<<name<<endl;
+}
