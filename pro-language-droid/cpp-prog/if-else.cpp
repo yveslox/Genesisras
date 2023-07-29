e9c5e2eebc008f71be293c7b6298a00bd2e65615
yveslox <yvesmole@gmail.com>
2023-07-29 12:36:34 +0200

    29/07/2023

---
+++ pro-language-droid/cpp-prog/if-else.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num = 15;
+    if(num == 15)
+    {
+        cout<<"num is equal to 15"<<endl;
+    }
+    else if(num == 13)
+    {
+        cout<<"n=num is equal to 13"<<endl;
+    }
+
+    else if(num == 14)
+    {
+        cout<<"num equal to 14"<<endl;
+    }
+    else 
+    {
+        cout<<"num : "<<num<<endl;
+    }
+}
