b4b67c1859c06bedc7c98c746a711d7af9bd26dd
yveslox <yvesmole@gmail.com>
2023-08-02 13:44:03 +0200

    02/08/2023

---
+++ pro-language-droid/cpp-prog/character-array.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char char_array[]= {'W','e','l','c','o','m','e'};
+
+    int i;
+    for(i = 0;i<7;i++)
+    {
+        cout<<char_array[i];
+    }
+    cout<<endl;
+
+    char name[50]="John";
+    cout<<name<<endl;
+}
