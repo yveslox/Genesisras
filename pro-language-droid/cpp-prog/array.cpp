69055dcd33d7d526e87eaad90ac82f559ba16093
yveslox <yvesmole@gmail.com>
2023-08-06 11:45:24 +0200

    06/08/2023

---
+++ pro-language-droid/cpp-prog/array.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num[5]={13,8,1,2,9};
+    for(int i= 0;i<5;i++)
+    {
+        cout<<num[i]<<" ";
+    }
+    cout<<endl;
+}
