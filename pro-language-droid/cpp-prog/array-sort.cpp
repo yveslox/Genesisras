8582696cd8cb19d092cb5c0e5f17c0e1347b3b01
yveslox <yvesmole@gmail.com>
2023-08-07 16:03:44 +0200

    07/08/2023

---
+++ pro-language-droid/cpp-prog/array-sort.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num[5]={3,8,1,2,9};
+    for(int i= 0;i<5;i++)
+    {
+        cout<<num[i]<<" ";
+    }
+    cout<<endl;
+}
