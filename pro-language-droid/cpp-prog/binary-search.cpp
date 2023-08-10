a50342f063fe143a762bf724d1de1d8783773325
yveslox <yvesmole@gmail.com>
2023-08-10 14:34:08 +0200

    10/08/2023

---
+++ pro-language-droid/cpp-prog/binary-search.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include <algorithm> 
+//#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+    int num[5]={3,8,1,2,9};
+    sort(num,num+5);
+    for(int i= 0;i<5;i++)
+    {
+        cout<<num[i]<<" ";
+    }
+    cout<<endl;
+    cout<<binary_search(num,num+5,3)<<endl;
+    cout<<binary_search(num,num+5,4)<<endl;
+}
