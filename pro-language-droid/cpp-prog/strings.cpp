38594abc0c55de6b8a54d28bdec64b69ada29a1f
yveslox <yvesmole@gmail.com>
2023-08-11 10:44:13 +0200

    11/08/2023

---
+++ pro-language-droid/cpp-prog/strings.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include <algorithm> 
+//#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+    string name ="Abc";
+    cout<<name<<endl;
+
+    char chararray[10]="john";
+    string str=chararray;
+    cout<<str<<endl;
+    for(int i=0;i<str.length();i++)
+    {
+        cout<<str[i]<<" ";
+    }
+    cout<<endl;
+    
+}
