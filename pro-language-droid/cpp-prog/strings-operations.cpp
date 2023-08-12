19da65259829b9b6dcf39790765dff514c55ee6d
yveslox <yvesmole@gmail.com>
2023-08-12 12:28:08 +0200

    12/08/2023

---
+++ pro-language-droid/cpp-prog/strings-operations.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include <algorithm> 
+//#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+    string s ="abc";
+    
+    s.append("z");
+    cout<<s<<endl;
+
+    string rev = string(s.rbegin(),s.rend());
+    cout<<rev<<endl;
+
+    string tempstr ="We think about food";
+    cout<<tempstr.substr(3,5)<<endl;
+
+    if(s.find("cd")!=string::npos)
+    {
+        int found = s.find("cd");
+        cout<<"substring found at"<<found<<"or"<<s.find("cd")<<endl;
+    }
+    else 
+    {
+        cout<<"substring not found"<<endl;
+    }
+    string b ="xyz";
+    s.append(b);
+    cout<<s<<endl;
+    
+}
