6975d41172d2716675adf8f2d3fba25ee6a520ff
yveslox <yvesmole@gmail.com>
2023-08-14 10:43:47 +0200

    14/08/2023

---
+++ pro-language-droid/cpp-prog/string-permu.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include <algorithm> 
+//#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+
+ string per="abc";
+ do{
+    cout<<per<<endl;
+ }while(next_permutation(per.begin(),per.end()));
+}
