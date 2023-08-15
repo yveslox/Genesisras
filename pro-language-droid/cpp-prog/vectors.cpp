b1a3333faaa962aa1a2ea4b4e8b735ce87edcb37
yveslox <yvesmole@gmail.com>
2023-08-15 11:25:43 +0200

    15/08/2023

---
+++ pro-language-droid/cpp-prog/vectors.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include <algorithm> 
+//#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+vector<int>mv;
+mv.push_back(4);
+mv.push_back(2);
+mv.push_back(9);
+mv.push_back(5);
+mv.push_back(1);
+
+for(vector<int>::iterator it=mv.begin();it!=mv.end();it++)
+{
+    cout<<*it<<" ";
+}
+cout<<endl;
+}
