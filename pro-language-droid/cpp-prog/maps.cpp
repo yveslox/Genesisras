bf92f37dd0cabfcbc5dd2b35c0603b52066238c3
yveslox <yvesmole@gmail.com>
2023-08-23 12:49:32 +0200

    23/08/2023

---
+++ pro-language-droid/cpp-prog/maps.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include <algorithm> 
+//#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+    map<int,int>mm;
+    mm[4]=44;
+    mm[1]=11;
+    mm[3]=33;
+    mm[5]=55;
+    mm[2]=22;
+
+ for(map<int,int>::iterator it = mm.begin();it!=mm.end();it++)
+ {
+    cout<<(*it).first<<" "<<(*it).second<<endl;
+ }
+ cout<<mm[2]<<endl;
+}    
