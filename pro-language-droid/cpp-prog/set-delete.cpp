eceaf4559e50bb202592e520a4c715ac012389a5
yveslox <yvesmole@gmail.com>
2023-08-27 13:01:40 +0200

    27/08/2023

---
+++ pro-language-droid/cpp-prog/set-delete.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include <algorithm> 
+//#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+    set<int>st;
+    st.insert(33);
+    st.insert(22);
+    st.insert(44);
+    st.insert(55);
+    st.insert(11);
+    
+ for(set<int>::iterator it = st.begin();it!=st.end();it++)
+ {
+    cout<<(*it)<<" ";
+ }
+ cout<<endl;
+ 
+ st.erase(33);
+ for(set<int>::iterator it = st.begin();it!=st.end();it++)
+ {
+    cout<<(*it)<<" ";
+ }
+ cout<<endl;
+ 
+
+}    
