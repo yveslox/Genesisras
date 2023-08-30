dba8771a2f35cdc3f34866035d881ab542274cd1
yveslox <yvesmole@gmail.com>
2023-08-30 12:49:20 +0200

    29/08/2023

---
+++ pro-language-droid/cpp-prog/stack.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+int main( )
+{
+    stack <int> mystack;
+    mystack.push(4);
+    mystack.push(3);
+    mystack.push(2);
+    mystack.push(1);
+
+    while(mystack.empty() == false)
+    {
+        cout<<mystack.top()<<" ";
+        mystack.pop();
+    }
+    cout<<endl;
+}
