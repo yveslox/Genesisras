467a1287beb08501cbacd0c28696da88aa699792
yveslox <yvesmole@gmail.com>
2023-09-04 11:29:48 +0200

    04/09/2023

---
+++ pro-language-droid/cpp-prog/fibonacci.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+int fibonacci(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main( )
+{
+  int i;
+  for(i = 1;i<=10;i++)
+  {
+    cout<<fibonacci(i)<<" ";
+  }
+  cout<<endl;
+}
