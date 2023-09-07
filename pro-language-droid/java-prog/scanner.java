caf6cceaa0f9853c873b04bda42992b19bec65a1
yveslox <yvesmole@gmail.com>
2023-09-07 12:32:36 +0200

    07/09/2023

---
+++ pro-language-droid/java-prog/scanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Scanner
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = input.nextLine();
+
+        System.out.println("Enter your age");
+        int age = input.nextInt();
+
+        System.out.println("Hello "+name);
+        System.out.println("Your age "+age);
+    }
+}
