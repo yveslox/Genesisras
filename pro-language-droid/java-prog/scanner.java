415cb07e2a5c9458a75a6100be85edce3026f617
yveslox <yvesmole@gmail.com>
2023-09-09 11:38:32 +0200

    08/09/2023

--- pro-language-droid/java-prog/scanner.java
+++ pro-language-droid/java-prog/scanner.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 class Scanner
 {
     public static void main(String args[])
---
+++ pro-language-droid/java-prog/sum.java
@@ -0,0 +1,19 @@
+//import java.util.scanner;
+import java.util.*;
+
+class sum 
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter fisrt number");
+        int x = input.nextInt();
+
+        System.out.println("Enter Second number");
+        int y = input.nextInt();
+
+        int sum = x + y;
+        System.out.println("Sum is : "+sum);
+        
+    }
+}
