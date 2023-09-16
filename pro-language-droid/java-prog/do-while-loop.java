10f50f8c90d53185dded18973de30ba6131daff5
yveslox <yvesmole@gmail.com>
2023-09-16 13:21:35 +0200

    16/09/2023

---
+++ pro-language-droid/java-prog/do-while-loop.java
@@ -0,0 +1,16 @@
+class do_while_loop
+{
+    public static void main(String args[])
+    {
+        int x = 5;
+        int i = 1;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=x);      
+                
+        
+    }
+}
