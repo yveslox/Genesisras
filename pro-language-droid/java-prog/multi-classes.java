09f00277bcd6e91f34cc7cb81a794a4e40c28648
yveslox <yvesmole@gmail.com>
2023-09-18 13:24:34 +0200

    18/09/2023

---
+++ pro-language-droid/java-prog/multi-classes.java
@@ -0,0 +1,14 @@
+class multi_classes 
+{
+    public static void main(String args[])
+    {
+        classSecond obj = new classSecond();
+        System.out.println(obj.x);
+        
+    }
+   
+}
+class classSecond
+{
+    int x = 5;
+}
