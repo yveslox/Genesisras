4a455300ab09acb2ee34e2fa7364e896c78ba99d
yveslox <yvesmole@gmail.com>
2023-09-25 13:31:43 +0200

    25/09/2023

---
+++ pro-language-droid/java-prog/math-class.java
@@ -0,0 +1,20 @@
+//import java.util.scanner;
+import java.util.*;
+
+class math_class 
+{
+    public static void main(String args[])
+    {
+    
+        System.out.println(Math.abs(-25.6));
+        System.out.println(Math.abs(25.6));
+        System.out.println(Math.ceil(25.6));
+        System.out.println(Math.floor(25.6));
+        System.out.println(Math.max(2,4));
+        System.out.println(Math.min(2,4));
+        System.out.println(Math.pow(2,4));
+        System.out.println(Math.sqrt(9));
+        
+        
+    }
+}
