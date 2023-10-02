4045a954aa6e7044bccd7f535ab6e051849a7240
yveslox <yvesmole@gmail.com>
2023-10-02 14:19:29 +0200

    02/10/2023

---
+++ pro-language-droid/java-prog/var-length.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+class var_length 
+{
+    public static void main(String args[])
+    {
+        int arr[]=new int [6];
+        int i;
+
+        for(i=0;i<arr.length;i++)
+        {
+        arr[i]=i;   
+        }
+       change(arr); 
+    }
+    static void change(int ...arr)
+    {
+        for(int j:arr)
+        System.out.println((j));
+    }
+
+}
