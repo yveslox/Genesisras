28ca156c6731aeed4d1698e5469e59f9dbd442a5
yveslox <yvesmole@gmail.com>
2023-10-18 14:04:54 +0200

    18/10/2023

---
+++ pro-language-droid/python-prog/operator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+num1 = 12
+num2 = 7
+
+sum = num1+num2
+diff = num1-num2
+product = num1*num2
+quotient = num1/num2
+remainder = num1%num2
+exponent = 3**2
+
+print ("sum is ",sum)
+print ("difference is ",diff)
+print ("Product is ",product)
+print ("quotient is ",quotient)
+print ("remainder is ",remainder)
+print("3^2 = ",exponent)
