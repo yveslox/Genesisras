a3010ce238a54e24c6954e3d16467ece2df77755
yveslox <yvesmole@gmail.com>
2023-10-19 18:38:38 +0200

    19/10/2023

---
+++ pro-language-droid/python-prog/comparison.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+a = 30
+b = 40
+
+if(a==b):
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+
+if(a>b):
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+
+
+if(a<b):
+    print("a is less than b")
+else:
+    print("a is not less than b")
+
+if(a>=b):
+    print("a is greater than or equal to b")
+else:
+    print("a is neither greater than nor equal to b")
+
+if(a==b):
+    print("a is less than or equal to b")
+else:
+    print("a is neither less than nor equal to b")
