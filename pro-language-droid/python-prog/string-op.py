cd8b7793fbcf17a22bb9ec475b5784ebb2e57e72
yveslox <yvesmole@gmail.com>
2023-10-23 15:25:10 +0200

    23/10/2023

---
+++ pro-language-droid/python-prog/string-op.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+str ="Learnong Python Programming"
+#complete string
+print(str)
+
+print(str[0])
+
+print(str[3:6])
+
+print(str[4:])
+
+print(str*2)
+
+print(str+"!!")
+
---
+++ pro-language-droid/python-prog/while-loop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#while loop
+num = 1
+while(num<=5):
+    print("num : ",num)
+    num = num +1
+    print("")
+
+#ele statement with the loop
+num = 1
+while(num<=5):
+    print("num : ",num)
+    num = num +1
+    print("")
+else:
+    print("Second loop finished")
