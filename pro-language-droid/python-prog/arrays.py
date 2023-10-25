774c9f533f6d8a1f27e28f00ae65544b1e748afd
yveslox <yvesmole@gmail.com>
2023-10-25 14:07:46 +0200

    25/10/2023

---
+++ pro-language-droid/python-prog/arrays.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+arr = []
+
+arr.append(2)
+arr.append(6)
+arr.append(1)
+arr.append(9)
+arr.append(3)
+
+print("arr elements :",arr)
+
+arr[2] = 5
+
+print("arr elements after updating array.",arr)
+
+arr.sort()
+print("arr elements after sorting",arr)
