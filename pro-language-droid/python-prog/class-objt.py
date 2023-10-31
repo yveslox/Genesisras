200965fd513ae95e7cbddae733acd452aa532423
yveslox <yvesmole@gmail.com>
2023-10-31 14:52:17 +0100

    31/10/2023

---
+++ pro-language-droid/python-prog/class-objt.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+class Books:
+    totalbooks = 0
+    def __init__(self,name,id):
+        self.name = name
+        self.id = id
+        Books.totalbooks += 1
+    def totalNumberOfbooks(self):
+        print("Total Books : ",Books.totalBooks)
+    def bookinformation(self):
+        print("Book name : ",self.name,",Book id : ",self.id)
+
+book1 = Books("python",12);
+book2 = Books("Kotlin",7);
+book1.bookinformation()
+book2.bookinformation()
+
+print("Total number of books : ",Books.totalbooks)
