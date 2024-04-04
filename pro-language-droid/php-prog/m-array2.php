a62da97b77d574172d3389f0771a7913994d83a5
yveslox <yvesmole@gmail.com>
2024-04-04 12:43:36 +0200

    projects-day394

---
+++ pro-language-droid/php-prog/m-array2.php
@@ -0,0 +1,28 @@
+<?php 
+$marks = array(
+    "merbin" => array(
+        "physics" => 35,
+        "maths" => 30,
+        "chemestry" =>39,
+    ),
+    "stalin" => array(
+        "physics" => 30,
+        "maths" => 32,
+        "chemestry" =>29,
+    ),
+    "angel" => array(
+        "physics" => 31,
+        "maths" => 22,
+        "chemestry" =>39,
+    ),
+);
+
+echo "Marks for merbin in physics :";
+echo $marks['merbin']['physics']."\n";
+
+echo "Marks for stalin in physics :";
+echo $marks['stalin']['maths']."\n";
+
+echo "Marks for angel in physics :";
+echo $marks['angel']['chemestry']."\n";
+?>
