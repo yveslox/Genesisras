a04f1f079e6110c271f440dab4b834e1fee20bb5
yveslox <yvesmole@gmail.com>
2024-04-20 12:58:15 +0200

    projects-day411

--- practice-code-blue/programme-python/for-loop.py
+++ practice-code-blue/programme-python/for-loop.py
@@ -6,7 +6,7 @@ print("Loop 1")
 
 for ch in name:
   print(ch),
-print("")
+print(" ")
   
 #iterate  using sequence index
 print("loop 2")
@@ -15,7 +15,7 @@ len=len(name)
 
 for i in range(len):
    print (name[i]),   
-print("")
+print(" ")
    
 #else statement with for loop
 print("loop 3")
@@ -23,6 +23,6 @@ print("loop 3")
 for ch in name:
    print(ch),
 else :
-   print("")
+   print(" ")
    print("Third loop completed")
    
