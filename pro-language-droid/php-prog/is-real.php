de2e57063f20e6c8afecb9a357700e85307de3f1
yveslox <yvesmole@gmail.com>
2024-05-23 15:04:32 +0200

    23/05/2024

---
+++ pro-language-droid/php-prog/is-real.php
@@ -0,0 +1,16 @@
+<?php 
+
+$var_real = 127.55;
+$var_int = 52;
+
+if(is_real($var_real))
+echo "This is a integer value."."\n";
+else 
+echo "This is not a integer value."."\n";
+
+if(is_real($var_int))
+echo "This is a integer value."."\n";
+else 
+echo "This is not a integer value."."\n";
+
+?>
