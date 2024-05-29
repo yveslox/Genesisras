d137d3cddc0884d4af5be849dcfc9e160961e182
yveslox <yvesmole@gmail.com>
2024-05-29 16:58:49 +0200

    projects-day449

--- pro-language-droid/php-prog/is-double.php
+++ pro-language-droid/php-prog/is-double.php
@@ -3,9 +3,9 @@
 $var_name = 256.63;
 
 if(is_double($var_name))
-echo "This is a double value."."\n";
+echo "This is a double value..."."\n";
 else 
-echo "This is not a double value."."\n";
+echo "This is not a double value..."."\n";
 
 
 ?>
