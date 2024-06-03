68eb97fbbb7e75c76ccfc35596a993d941ef97e2
yveslox <yvesmole@gmail.com>
2024-06-03 17:55:12 +0200

    03/06/2024

---
+++ pro-language-droid/php-prog/is-num.php
@@ -0,0 +1,16 @@
+<?php 
+
+$var_alphanumeric = "gf3654";
+$var_numeric = 365;
+
+if(is_numeric($var_alphanumeric))
+echo "This is a numeric value"."\n";
+else 
+echo "This is not a numeric value"."\n";
+
+if(is_numeric($var_numeric))
+echo "This is a numeric value"."\n";
+else 
+echo "This is not a numeric value"."\n";
+
+?>
