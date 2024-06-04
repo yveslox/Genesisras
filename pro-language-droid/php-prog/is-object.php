2c9d90fa7c583e5b4298a93e8aea0356199fc87c
yveslox <yvesmole@gmail.com>
2024-06-04 15:21:45 +0200

    04/06/2024

---
+++ pro-language-droid/php-prog/is-object.php
@@ -0,0 +1,16 @@
+<?php 
+
+$obj=Array("Physics","Chemestry","Mathematics");
+$var_int = 890;
+
+if(is_object($obj))
+echo "This is object"."\n";
+else 
+echo "This is not object"."\n";
+
+if(is_object($var_int))
+echo "This is object"."\n";
+else 
+echo "This is not object"."\n";
+
+?>
