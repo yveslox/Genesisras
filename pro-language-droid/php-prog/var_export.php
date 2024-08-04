e7ac0061d16521e52235580e1cc539afd58566e5
yveslox <yvesmole@gmail.com>
2024-08-04 16:15:47 +0200

    04/08/2024

---
+++ pro-language-droid/php-prog/var_export.php
@@ -0,0 +1,17 @@
+<?php 
+
+$var_name1 = 126;
+$var_name2 = 'a695';
+$var_name3 = '1258';
+$var_name4 = 'softytech.com';
+$var_name5 = 15.99;
+$var_name6 = +9854.66;
+
+echo var_export($var_name1)."\n";
+echo var_export($var_name2)."\n";
+echo var_export($var_name3)."\n";
+echo var_export($var_name4)."\n";
+echo var_export($var_name5)."\n";
+echo var_export($var_name6)."\n";
+
+?>
