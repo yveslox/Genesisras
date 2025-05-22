859830b3be03afa43f2cc6e2300f3d255bcd159c
yveslox <yvesmole@gmail.com>
2025-05-22 16:38:57 +0200

    22/05/2025

--- practice-code-orange/programme-python/user-input.py
+++ practice-code-orange/programme-python/user-input.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-name = raw_input("please enter name : ")
-age = int(raw_input("please enter age: ")
+name = input("please enter name : ")
+age = input("please enter age: ")
 
- print(" Hello :",name)
+print("Hello :",name)
 
- print("your age is :",age)
-
-
+print("your age is :",age)
